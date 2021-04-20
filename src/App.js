470f913be1a03495ec78d8c218ec003654e3b643
JPRR44 <juanp.robles@hotmail.com>
2021-04-20 16:54:30 -0500

    Feat: Added router and main route to HomePage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import  HomePage  from './containers/home';
 
 function App() {
   return (
-    <div>JPAR</div>
+    <div className="App">
+      <Router>
+        <div className='content'>
+          <Switch>
+            <Route exact path="/" render={() => <HomePage />} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
