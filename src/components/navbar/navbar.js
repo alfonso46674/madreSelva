808c57b95a11825ac9cd9b3810eeb1f4204542fc
BuildTools <is713932@iteso.mx>
2021-05-25 00:08:40 -0500

    Fix: Show logo in navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,11 +3,13 @@ import { Row, Col } from 'react-bootstrap';
 import './navbar.scss';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
+import logo from '../../assets/simple_logo.png';
 const Navbar = ({ user, setUser }) => {
 
+  // console.log(logo);
   return (
     <Row className='Navbar'>
-      <Col sm={8} className='Slogan'>logo<img src='../../../logo.png'></img></Col>
+      <Col sm={8} className='Slogan'><img src={logo} alt='LOGO.PNG' width="50" height="50"></img> </Col>
       <Col sm={1} className='Client'>
         <Button><Link to='/' className='navbar-text'>Inicio</Link></Button>
       </Col>
