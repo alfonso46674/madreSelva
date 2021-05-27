7daa839aa78753cab846bb7c74c1a13d628e2a2f
BuildTools <is713932@iteso.mx>
2021-05-27 11:29:11 -0500

    Style: Expand abstract column in admin

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -109,7 +109,7 @@ const Admin = () => {
           <td>{author}</td>
           <td><Button onClick={obtainIdAndOpenModal}>{status}</Button></td>
           <td>{category}</td>
-          <td >{abstract}</td>
+          <td id='abstract'>{abstract}</td>
           </tr>
       )
     })
