0853f08db99b3e6bb7d663fe5c7fac1a0c41b23b
BuildTools <is713932@iteso.mx>
2021-05-27 03:24:14 -0500

    Feat: Download pdf from card button

--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -2,14 +2,14 @@ import {React,useEffect,useState} from 'react';
 import { Container, Row, Col, Form,Card } from 'react-bootstrap';
 import './videos.scss';
 import Button from '@material-ui/core/Button';
-import { DropdownButton, Dropdown } from 'react-bootstrap'
+import FileDownload from 'js-file-download'
 import ReactPlayer from 'react-player';
 import axios from 'axios'
 
 const Videos = () => {
   const urlAcceptedSubmissions = 'http://localhost:8080/api/submissions/accepted'
   const urlSearchAcceptedSubmissions = 'http://localhost:8080/api/submissions/search'
-
+  const urlDownloadFile = 'http://localhost:8080/api/files/download?id='
  
 
   //obtener accepted submissions al cargar la pagina
@@ -48,11 +48,13 @@ const Videos = () => {
           <Card >
           <Card.Header as='h5'>{title}</Card.Header>
           <Card.Body>
+            {/* Poner el id y ocultarlo para asi poder accesarlo desde un event trigger, que seria el boton de descarga */}
+            <p hidden>{id}</p>
             <Card.Title>{author}</Card.Title>
             <Card.Text>
               {abstract}
             </Card.Text>
-            <Button variant="contained" color="primary" className='SubmitBtn'>Descargar PDF</Button>
+            <Button onClick={downloadPDF} variant="contained" color="primary" className='SubmitBtn'>Descargar PDF</Button>
           </Card.Body>
           <Card.Footer className="text-muted">{category}</Card.Footer>
         </Card>
@@ -63,6 +65,7 @@ const Videos = () => {
           <Card className='card'>
           <Card.Header as='h5'>{title}</Card.Header>
           <Card.Body>
+            <p hidden>{id}</p>
             <Card.Title>{author}</Card.Title>
             <Card.Text>
               {abstract}
@@ -75,6 +78,29 @@ const Videos = () => {
       }
     })
   }
+
+  const downloadPDF = (e) =>{
+    let eventId = e.target.parentElement.parentElement.firstChild.innerHTML
+
+    let titleEvent = e.target.parentElement.parentElement.previousSibling.innerHTML
+
+    axios({
+      url:urlDownloadFile+eventId,
+      method:'GET',
+      responseType:'blob'
+    })
+    .then(res => {
+      console.log(res);
+      //download file to browser
+      FileDownload(res.data,`${titleEvent}.pdf`)
+      //https://stackoverflow.com/questions/41938718/how-to-download-files-using-axios
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+
 
   //search data
   const [searchData,setData] = useState({
